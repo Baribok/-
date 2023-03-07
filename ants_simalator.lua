5fd787712a6ece02d9f171c0e22daff6a104e9ec
Baribok <102378610+Baribok@users.noreply.github.com>
2023-03-08 00:24:52 +0300

    ddd

---
+++ ants_simalator.lua
@@ -0,0 +1,43 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Robojini/Tuturial_UI_Library/main/UI_Template_1"))()
+
+local Window = Library.CreateLib("Ants Simulator by baribok1", "RJTheme3")
+
+local Tab = Window:NewTab("инфо")
+
+local Section = Tab:NewSection("инфо все в консоли")
+
+Section:NewButton("скрипт от игрока baribok1 ", "скрипт от игрока baribok1", function()
+    print("")
+end)
+
+Section:NewButton("дискорд Greenn#4077", "дискорд Greenn#4077", function()
+    print("")
+end)
+
+Section:NewButton("??", "??", function()
+    game:service('Players').LocalPlayer:Kick('Кик бехиватор!')
+end)
+
+local Tab = Window:NewTab("антс симулятор")
+
+local Section = Tab:NewSection("ант симулятор")
+
+Section:NewButton("All tokens tp", "all tokens tp to you", function()
+    for i,v in pairs(game:GetDescendants()) do
+		if v.Name == 'Token' then
+		v.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
+		end
+		end
+end)
+
+Section:NewSlider("SliderText", "SliderInfo", 500, 16, function(s) 
+    game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = s
+end)
+
+Section:NewKeybind("KeybindText", "KeybindInfo", Enum.KeyCode.F, function()
+	for i,v in pairs(game:GetDescendants()) do
+		if v.Name == 'Token' then
+		v.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
+		end
+		end
+end)
