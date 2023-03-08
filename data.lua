4e53abdb7ab1e61c23dea65335ffde70696b34c5
Baribok <102378610+Baribok@users.noreply.github.com>
2023-03-08 14:06:50 +0300

    Create data.lua

---
+++ data.lua
@@ -0,0 +1,9 @@
+userids = {
+-- [айди игрока в роблоксе] = true,
+[1601778167] = true,
+[4352255381] = true
+}
+return userids
+
+-- айди игрока это цифры после /users/
+-- https://www.roblox.com/users/123456789/profile
